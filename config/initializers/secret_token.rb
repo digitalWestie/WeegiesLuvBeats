c60691b90015f6da542a22fcd620485f751d81b3
digitalWestie <giannichan@gmail.com>
2012-04-27 23:52:39 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeegieBeats::Application.config.secret_token = '5bfd8cd7cea233daee02140c9773bed6bdc2a16cdf7438a4aefa79fb070013b025c39e8692bf6b2afda89e060b38b7ef3d8034da1446ecab045e9a10b385f708'
