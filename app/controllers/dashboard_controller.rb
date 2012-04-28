d915f1d341015c9effa3f0937420d9a47c813986
digitalWestie <giannichan@gmail.com>
2012-04-28 15:51:41 +0100

    help

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,15 @@
 class DashboardController < ApplicationController
 
   def index
-    @user = Rockstar::User.new(session[:username])
-    @artists = Artist.rank_by_top_tags(@user, 0.25)#, tolerance=0.25
+    #@user = Rockstar::User.new(session[:username])
+    @user = Rockstar::User.new('digitalWestie')
+    artists = Artist.rank_by_top_tags(@user, 0.25)#, tolerance=0.25
+    #    artists = Artist.have_data.all[0 .. 20]
+    @artists = []
+    for a in artists
+      a = Rockstar::Artist.new(a.name, :include_info => true)
+      @artists << a
+    end
   end
 
 end
