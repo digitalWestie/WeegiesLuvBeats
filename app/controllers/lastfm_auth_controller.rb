6e4f50195851fba6ba16d56b13825f7bef7ec1fb
digitalWestie <giannichan@gmail.com>
2012-04-28 05:03:47 +0100

    go go go

--- app/controllers/lastfm_auth_controller.rb
+++ app/controllers/lastfm_auth_controller.rb
@@ -1,13 +1,13 @@
 class LastfmAuthController < ApplicationController
 
-  def authorize
+  def get_token
     redirect_to 'http://www.last.fm/api/auth/?api_key=267346c6269500db6f77d00088cf4068'
   end
 
   def callback
     if params[:token]    
-      #redirect_to 'http://ws.audioscrobbler.com/2.0/?method=album.getSession'
-      render :text => params[:token]       
+      redirect_to 'http://ws.audioscrobbler.com/2.0/?method=auth.getsession&token=#{params[:token]}&api_key=267346c6269500db6f77d00088cf4068&api_sig=246cd98ffc2cf2e80efd02658df2804a'
+      #render :text => params[:token]       
     else
       render :text => "Borked! Couldnae login!"
     end
