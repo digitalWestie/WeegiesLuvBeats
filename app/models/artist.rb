75746753a59abc94ab8cb79c4cac7119433ccc34
digitalWestie <giannichan@gmail.com>
2012-04-28 14:15:09 +0100

    algorithm

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,11 +10,45 @@ class Artist < ActiveRecord::Base
     lastfm_rec = user.recommended_artists(key)
     names = lastfm_rec.collect { |a| a.name }
     artists = Artist.find_all_by_name(names)
+  end
 
-    fm_tags = user.top_tags.collect { |t| t.name }
-    tags = Tag.find_all_by_name(fm_tags)
-    artists.concat(tags.collect { |t| t.artists }.flatten)
+  def self.rank_by_top_tags(user, tolerance=0.25)
+    #fm_tags = user.top_tags.collect { |t| t.name }
+    #tags = Tag.find_all_by_name(fm_tags)
+    #artists = tags.collect { |t| t.artists }.flatten
+    #return artists
+    tags = []
+    user.top_artists.each {|artist| tags.concat(artist.top_tags[0 .. 4]) }
+    ranking = {}
+    tags.uniq.each { |tag| ranking.merge!({tag.name => 0}) }
+    tags.each { |tag| ranking[tag.name] += tag.count.to_i }
+
+    rank_arr = ranking.to_a
+    max = rank_arr.collect {|e| e[1] }.max #get highest tag count
+    rank_arr.each {|e| e[1] = e[1].to_f/max }
+    rank_arr.delete_if {|e| e[1] > tolerance } # keep crappy ones to get rid of
+    rank_arr.each {|e| ranking.delete(e[0]) }    
+
+    artists = Tag.find_all_by_name(ranking.keys).collect {|e| e.artists }.flatten
+    artists.keep_if {|a| artists.count(a) > 2 }
+    artists.uniq!
+    artists.sort { |a,b| b.rank(ranking) <=> a.rank(ranking) }
+    #return [artists.collect { |b| b.rank(ranking) }, artists]
     return artists
+  end
+
+  def rank(rankings)
+    score = 0
+    for tag in self.tags
+      tag_score = 0
+      unless rankings[tag.name].nil?
+        tag_score = rankings[tag.name] 
+        tag_score = tag_score * tag.associated_tags.find_by_artist_id(self.id).count
+      end
+      score += tag_score
+    end
+
+    return score
   end
 
 	scope :have_data, where(:has_entry => true)
